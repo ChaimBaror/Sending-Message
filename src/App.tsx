0697884025f1ade8710ba3149a54cbcd4b7a0c2e
Chaim Bar-or <chaimb@ravtech.local>
2024-04-04 17:20:03 +0300

    fix the list mobile

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,11 @@ import { Cog6ToothIcon } from '@heroicons/react/20/solid';
 import { useTranslation } from 'react-i18next';
 import UserList from './components/ListUser/UserList';
 
-const usersList = [
-  { username: ' חיים בר', phone: '+972 0527158077' },
-  { username: 'איתיאל', phone: '+972 52-763-8404' },
-  { username: 'יואל', phone: '+972 50-312-1062' },
-  { username: 'מנחם', phone: '+972 546684821' },
-  { username: 'שמעון דורמנו', phone: '+972 585993447' },
-];
-
 const App = () => {
   const { t, i18n } = useTranslation();
+  useEffect(() => {
+    setDirection(i18n.language === 'he' ? 'rtl' : 'ltr');
+  }, [i18n]);
   const { preferredCountry } = useSettings();
 
   const [direction, setDirection] = useState('rtl');
@@ -26,10 +21,10 @@ const App = () => {
   const [isPhoneValid, setIsPhoneValid] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [isOpenModalSettings, setIsOpenModalSettings] = useState(false);
-  const [users, setUsers] = useState(usersList);
-  const [selectedUser, setSelectedUser] = useState(usersList[0]);
-  const [message, setMessage] = useState('');
-  const [greeting, setGreeting] = useState('');
+  const [selectedUser, setSelectedUser] = useState({ username: '', phone: '' });
+  const [message, setMessage] = useState('hi there!');
+  const [greeting, setGreeting] = useState(t('app.greeting'));
+  const [username, setUsername] = useState(selectedUser.username);
 
   const handleChange = (numberValue: string, country: CountryData) => {
     if (!numberValue.startsWith('+')) numberValue = `+${numberValue}`;
@@ -37,28 +32,27 @@ const App = () => {
     setIsPhoneValid(isValidPhone);
   };
 
-  useEffect(() => {
-    setDirection(i18n.language === 'he' ? 'rtl' : 'ltr');
-  }, [i18n]);
-
   const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(e.target.value);
   };
 
   const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+  const handleGreetingChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setGreeting(e.target.value);
   };
-
-  useEffect(() => {
-    setGreeting(`${t('app.greeting')} ${selectedUser.username}!`);
-  }, [selectedUser]);
 
   useEffect(() => {
     setToWhatsapp();
   }, [message, greeting]);
 
+  useEffect(() => {
+    setUsername(selectedUser.username);
+  }, [selectedUser]);
+
   const setToWhatsapp = () => {
-    const fullMessage = `${greeting} ${message}`;
+    const fullMessage = `${greeting} ${username} ${message}`;
     const isValidPhone = isValidPhoneNumber(selectedUser.phone, preferredCountry);
     setIsPhoneValid(isValidPhone);
     setWhatsAppUrl(isValidPhone ? selectedUser.phone : '', fullMessage);
@@ -69,46 +63,59 @@ const App = () => {
     <div className='relative h-screen w-screen overflow-hidden bg-gray-100'>
       <SettingsModal open={isOpenModalSettings} close={() => setIsOpenModalSettings(false)} />
       <button
-        className='fixed top-5 right-5 cursor-pointer rounded-xl p-4 focus-visible:outline focus-visible:outline-emerald-600'
+        className='z-50 fixed top-5 right-5 cursor-pointer rounded-xl p-4 focus-visible:outline focus-visible:outline-emerald-600'
         onClick={() => setIsOpenModalSettings(true)}
       >
         <Cog6ToothIcon className='h-5 w-5 text-emerald-500' />
       </button>
-      <div className="absolute z-10 container mx-auto p-4">
-        <h1 className="text-2xl font-bold mb-4">{t('app.userslist')}</h1>
-        <UserList users={users} setSelectedUser={setSelectedUser} />
+      <div className='absolute z-10 mx-auto'>
+        <h1 className='mb-4 text-2xl font-bold'>{t('app.userslist')}</h1>
+        <UserList setSelectedUser={setSelectedUser} />
       </div>
       <div className='absolute top-0 left-0 aspect-1 w-80 -translate-x-1/2 -translate-y-1/2 rounded-full bg-emerald-300 blur-lg' />
       <div className='absolute right-0 bottom-0 aspect-1 w-80 translate-x-2/3 translate-y-2/3 rounded-full bg-emerald-300 blur-lg' />
       <div className='relative mx-auto h-full max-w-xs'>
-        <div className='flex h-full flex-col items-center justify-center gap-4' >
+        <div className='text-center flex h-full flex-col items-center justify-center gap-4'>
           <h1 className='text-3xl font-bold' dangerouslySetInnerHTML={{ __html: t('app.title') }}></h1>
-          <PhoneField phoneUser={selectedUser.phone} preferredCountry={preferredCountry} onChange={handleChange} isValid={isPhoneValid} />
-          <div dir={direction}>
-            <h2 className='text-lg font-medium' >
+          <PhoneField
+            phoneUser={selectedUser.phone}
+            preferredCountry={preferredCountry}
+            onChange={handleChange}
+            isValid={isPhoneValid}
+          />
+          <div className='flex flex-col gap-4' dir={direction}>
+            <h2 className='text-lg font-medium'>
               <input
-                type="text"
+                type='text'
                 value={greeting}
-                onChange={handleUsernameChange}
-                placeholder="Username"
-                className="border border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500"
+                onChange={handleGreetingChange}
+                placeholder='Username'
+                className='rounded-md border border-gray-300 p-2 focus:border-blue-500 focus:outline-none'
               />
+              <input
+                type='text'
+                value={username}
+                onChange={handleUsernameChange}
+                placeholder='Username'
+                className='rounded-md border border-gray-300 p-2 focus:border-blue-500 focus:outline-none'
+              />
+              <textarea
+                defaultValue={message}
+                onChange={handleMessageChange}
+                placeholder='Type your message...'
+                className='rounded-md border border-gray-300 p-2 focus:border-blue-500 focus:outline-none'
+                rows={4}
+              ></textarea>
             </h2>
-            <textarea
-              value={message}
-              onChange={handleMessageChange}
-              placeholder="Type your message..."
-              className="border border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500"
-              rows={4}
-            ></textarea>
           </div>
           <a
             aria-disabled={!isPhoneValid}
             href={whatsAppUrl}
             target='_blank'
             rel='noopener noreferrer'
-            className={`flex w-full items-center justify-center rounded-3xl bg-emerald-500 py-2 font-medium text-white focus-visible:outline focus-visible:outline-emerald-600 ${(!isPhoneValid || isLoading) && 'pointer-events-none opacity-60'
-              }`}
+            className={`flex w-full items-center justify-center rounded-3xl bg-emerald-500 py-2 font-medium text-white focus-visible:outline focus-visible:outline-emerald-600 ${
+              (!isPhoneValid || isLoading) && 'pointer-events-none opacity-60'
+            }`}
             onClick={() => setIsLoading(true)}
           >
             {isLoading && (
@@ -132,11 +139,7 @@ const App = () => {
         <div className='absolute bottom-10 w-full'>
           <p className='w-full text-center text-xs font-semibold text-gray-500'>
             {t('app.footer.madeWidth')} <span className='text-red-500'>❤</span> {t('app.footer.by')}{' '}
-            <a
-              target='_blank'
-              rel='noopener noreferrer'
-              className='text-emerald-500 hover:underline'
-            >
+            <a target='_blank' rel='noopener noreferrer' className='text-emerald-500 hover:underline'>
               @ChaimBaror
             </a>
           </p>
--- src/components/ListUser/UserList.tsx
+++ src/components/ListUser/UserList.tsx
@@ -1,28 +1,125 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface User {
-  username: string;
+  name: {
+    first: string;
+    last: string;
+  };
   phone: string;
+  picture?: {
+    thumbnail: string;
+    large: string;
+  };
 }
-
 interface UserListProps {
-  users: User[];
-  setSelectedUser: (user: User) => void;
+  setSelectedUser: (user: { username: string; phone: string }) => void;
 }
+const UserList: React.FC<UserListProps> = ({ setSelectedUser }) => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-const UserList: React.FC<UserListProps> = ({ users, setSelectedUser }) => {
+  useEffect(() => {
+    getData();
+  }, []);
+
   const handleUserClick = (user: User) => {
-    setSelectedUser(user);
+    setSelectedUser({ username: `${user.name.first} ${user.name.last}`, phone: user.phone });
   };
 
+  const getData = async () => {
+
+    const results = [
+      { name: { first: 'חיים', last: ' בר-אור' }, phone: '+972 0527158077' },
+      { name: { first: 'איתיאל', last: 'ראובן' }, phone: '+972 52-763-8404' },
+      { name: { first: 'יואל', last: 'פריד' }, phone: '+972 50-312-1062' },
+      { name: { first: 'מנחם', last: 'פיקסלר' }, phone: '+972 546684821' },
+      { name: { first: 'שמעון', last: ' דורמנו' }, phone: '+972 585993447' },
+    ];
+
+    setUsers(results);
+  };
+
+  const getRandomAvtar: () => string = () => {
+    return 'https://randomuser.me/api/portraits/men/' + Math.floor(Math.random() * 10) + '.jpg';
+  };
+
+  const filterData = (searchTerm = '') => {
+    return users.filter(
+      (user) =>
+        user.name.first.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        user.name.last.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredUsers = filterData(searchTerm);
+
+  const handleSelectChange = (e) => {
+    const user = filteredUsers.find(user => user.phone === e.target.value);
+    if (user)
+    setSelectedUser({ username: `${user.name.first} ${user.name.last}`, phone: user.phone });
+
+  }
+
   return (
-    <div className="flex flex-col space-y-4">
-      {users.map((user, index) => (
-        <div key={index} className="flex items-center space-x-4" onClick={() => handleUserClick(user)}>
-          <div className="font-bold">{user.username}</div>
-          <div>{user.phone}</div>
-        </div>
-      ))}
+    <div className='container w-80 overflow-hidden rounded-lg bg-white shadow-md '>
+      <div className='hidden p-4 lg:block'>
+        <header className='header relative flex bg-white p-8 shadow-md'>
+          <input
+            type='text'
+            id='filter'
+            placeholder='Search by name and/or location'
+            value={searchTerm}
+            onChange={handleInputChange}
+            className='w-full rounded-full border-0 bg-gray-200 px-6 py-4 focus:outline-none'
+          />
+          <i className='fa-solid fa-magnifying-glass'></i>
+        </header>
+        <ul id='result' className='user-list m-0 h-96 list-none overflow-y-auto bg-white p-0'>
+          {filteredUsers.length > 0 ? (
+            filteredUsers.map((user, index) => (
+              <li
+                key={index}
+                onClick={() => handleUserClick(user)}
+                className='flex items-center border-b border-gray-300 py-5 px-4'
+              >
+                <img
+                  src={'https://randomuser.me/api/portraits/men/' + index + '.jpg'}
+                  alt={user.name.first}
+                  className='h-12 w-12 rounded-full object-cover'
+                />
+                <div className='user-info ml-4'>
+                  <h4 className='text-base font-semibold'>
+                    {user.name.first} {user.name.last}
+                  </h4>
+                  <p className='text-sm'>{user.phone}</p>
+                </div>
+              </li>
+            ))
+          ) : (
+            <li className='py-5 px-4'>
+              <h3>Loading...</h3>
+            </li>
+          )}
+        </ul>
+      </div>
+      <div className="relative mb-2 block flex items-center p-4 lg:hidden">
+      <select 
+        required
+        className="w-64 cursor-pointer appearance-none rounded-lg border border-gray-200 bg-white px-3 py-2 text-black/70 outline-blue-600/50 transition-all invalid:text-black/30 hover:border-blue-600/30"
+        onChange={(e) => handleSelectChange(e)}
+      >
+        <option value="" disabled>Select an option</option>
+        {filteredUsers.length > 0 && filteredUsers.map((user, index) => (
+          <option key={index} value={user.phone} className="cursor-pointer">
+            {user.name.first} {user.name.last}
+          </option>
+        ))}
+      </select>
+    </div>
     </div>
   );
 };
